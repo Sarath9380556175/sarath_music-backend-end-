3264c0fbaf79a1c5714acf4887a353f1caed74e1
Bujala sarath kumar reddy <75170692+Sarath9380556175@users.noreply.github.com>
2021-11-18 11:41:37 +0530

    Update music.js

--- controllers/music.js
+++ controllers/music.js
@@ -5,18 +5,18 @@ exports.getsongsbylanguage=(req,res)=>
    const request=req.body;
 
    const language=request.language;
-   
+
    const page=request.page ? request.page :1; //ternary operator
-   const countperpage=5;
+   const countperpage=4;
    let startindex=(page*countperpage)-countperpage;
  let endindex=(page*countperpage);
-   
 
    songs.find({language:language})
 
    .then(response=>{
-        const filterpage=response.slice(startindex,endindex);
-      const pagecount=Math.ceil(response.length/5);
+      
+      const filterpage=response.slice(startindex,endindex);
+      const pagecount=Math.ceil(response.length/4);
       let pagecountarr=[];
       for(i=1;i<=pagecount;i++)
       {
@@ -34,9 +34,24 @@ exports.getsongsbyname=(req,res)=>{
 
    const songname=request.songname;
 
+   const page=request.page ? request.page :1; //ternary operator
+   const countperpage=2;
+   let startindex=(page*countperpage)-countperpage;
+ let endindex=(page*countperpage);
+
    songs.find({songname:songname})
 
-   .then(response=>{response.length!==0?res.status(200).json({songnames:response,issongexist:true}):res.status(200).json({songnames:response,issongexist:false})})
+   .then(response=>{
+      
+      const filterpage=response.slice(startindex,endindex);
+      const pagecount=Math.ceil(response.length/2);
+      let pagecountarr=[];
+      for(i=1;i<=pagecount;i++)
+      {
+          pagecountarr.push(i);
+      }
+      
+      response.length!==0?res.status(200).json({songnames:filterpage,issongexist:true,pagecount:pagecount,pagecounts:pagecountarr}):res.status(200).json({songnames:filterpage,issongexist:false,pagecount:pagecount,pagecounts:pagecountarr})})
 
    .catch(err=>res.status(500).json({error:err}));
 }
@@ -44,11 +59,28 @@ exports.getsongsbyname=(req,res)=>{
 exports.findbymoviename=(req,res)=>{
    const request=req.body;
 
+  
+
    const moviename=request.moviename;
+   const page=request.page ? request.page :1; //ternary operator
+   const countperpage=2;
+   let startindex=(page*countperpage)-countperpage;
+ let endindex=(page*countperpage);
+
 
    songs.find({moviename:moviename})
 
-   .then(response=>{response.length!==0?res.status(200).json({moviesongs:response,ismovieexist:true}):res.status(200).json({moviesongs:response,ismovieexist:false})})
+   .then(response=>{
+      
+      const filterpage=response.slice(startindex,endindex);
+      const pagecount=Math.ceil(response.length/2);
+      let pagecountarr=[];
+      for(i=1;i<=pagecount;i++)
+      {
+          pagecountarr.push(i);
+      }
+
+      response.length!==0?res.status(200).json({moviesongs:filterpage,ismovieexist:true,pagecount:pagecount,pagecounts:pagecountarr}):res.status(200).json({moviesongs:filterpage,ismovieexist:false,pagecount:pagecount,pagecounts:pagecountarr})})
 
    .catch(err=>res.status(500).json({error:err}))
 }
@@ -58,9 +90,25 @@ exports.findbymusicdirector=(req,res)=>{
 
    const musicdirector=request.musicdirector;
 
+   const page=request.page ? request.page :1; //ternary operator
+   const countperpage=2;
+   let startindex=(page*countperpage)-countperpage;
+ let endindex=(page*countperpage);
+
+  
    songs.find({music:musicdirector})
 
-   .then(response=>{response.length!==0?res.status(200).json({musicdirector:response,isdirectorexist:true}):res.status(200).json({musicdirector:response,isdirectorexist:false})})
+   .then(response=>{
+      
+      const filterpage=response.slice(startindex,endindex);
+      const pagecount=Math.ceil(response.length/2);
+      let pagecountarr=[];
+      for(i=1;i<=pagecount;i++)
+      {
+          pagecountarr.push(i);
+      }
+      
+      response.length!==0?res.status(200).json({musicdirector:filterpage,isdirectorexist:true,pagecount:pagecount,pagecounts:pagecountarr}):res.status(200).json({musicdirector:filterpage,isdirectorexist:false,pagecount:pagecount,pagecounts:pagecountarr})})
 
    .catch(err=>res.status(500).json({error:err}))
 }
@@ -71,12 +119,25 @@ exports.findbysingername=(req,res)=>{
 
    const singername=request.singername;
 
-   
+   const page=request.page ? request.page :1; //ternary operator
+   const countperpage=2;
+   let startindex=(page*countperpage)-countperpage;
+ let endindex=(page*countperpage);
+
 
    songs.find({singers:singername})
 
-   .then(response=>{response.length!==0?res.status(200).json({singernames:response,issingerexist:true}):res.status(200).json({singernames:response,issingerexist:false})})
+   .then(response=>{
+      
+      const filterpage=response.slice(startindex,endindex);
+      const pagecount=Math.ceil(response.length/2);
+      let pagecountarr=[];
+      for(i=1;i<=pagecount;i++)
+      {
+          pagecountarr.push(i);
+      }
+      
+      response.length!==0?res.status(200).json({singernames:filterpage,issingerexist:true,pagecount:pagecount,pagecounts:pagecountarr}):res.status(200).json({singernames:filterpage,issingerexist:false,pagecount:pagecount,pagecounts:pagecountarr})})
 
    .catch(err=>res.status(500).json({error:err}))
 }
-
